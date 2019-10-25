c7ac3e2c18fb473ec5afad70e0af1d6253e89310
ASO1701020 <1701020@st.asojuku.ac.jp>
2019-10-25 12:43:28 +0900

    グループ作成機能追加

--- app/src/main/java/jp/ac/asojuku/st/chirusapo/MainActivity.kt
+++ app/src/main/java/jp/ac/asojuku/st/chirusapo/MainActivity.kt
@@ -1,12 +1,154 @@
 package jp.ac.asojuku.st.chirusapo
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
+import android.content.DialogInterface
+import android.content.Intent
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+import jp.ac.asojuku.st.chirusapo.apis.Api
+import jp.ac.asojuku.st.chirusapo.apis.ApiError
+import jp.ac.asojuku.st.chirusapo.apis.ApiParam
+import jp.ac.asojuku.st.chirusapo.apis.ApiPostTask
+import kotlinx.android.synthetic.main.layout_group_create.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        groupCreate()
     }
+
+    // グループ作成
+    private fun groupCreate(){
+        /*
+        val factory = LayoutInflater.from(this)
+        val inputView = factory.inflate(R.layout.layout_group_create, null)
+        */
+
+        val inputView = View.inflate(this, R.layout.layout_group_create, null)
+
+        //関連付け
+        val group_id = inputView.findViewById(R.id.group_id) as TextInputLayout
+        val group_name = inputView.findViewById(R.id.group_name) as TextInputLayout
+
+        //Dialog生成
+        val alertDlg = AlertDialog.Builder(this)
+            .setTitle("グループ作成")
+            .setView(inputView)
+            .setPositiveButton(
+                "作成"
+            ) { dialog, which ->
+                // きむきむと一緒
+                val token = "5t0DUpyH4yrgPp8tv"
+                val groupId = group_id.editText?.text.toString()
+                val groupName = group_name.editText?.text.toString()
+
+                // APIとの通信を行う
+                ApiPostTask{
+                    // 処理した結果が帰ってくる
+                    if (it == null) {
+                        ApiError.showToast(this, ApiError.CONNECTION_ERROR, Toast.LENGTH_SHORT)
+                    }
+                    //なにかしら返答があった場合
+                    else {
+                        //statusを取得する
+                        when (it.getString("status")) {
+                            "200" -> {
+                                val belongGroup = it.getJSONObject("data").getJSONArray("belong_group")
+                                for (i in 0 until belongGroup.length()) {
+                                    val groupInfo = belongGroup.getJSONObject(i)
+                                    val groupInfoId = groupInfo.getString("group_id")
+                                    val groupInfoName = groupInfo.getString("group_name")
+
+                                    // Log.d("TEST", groupInfoId)
+                                    // Log.d("TEST", groupInfoName)
+                                }
+                            }
+                            "400" -> {
+                                val errorArray = it.getJSONArray("message")
+                                for (i in 0 until errorArray.length()) {
+                                    when (errorArray.getString(i)) {
+                                        ApiError.VALIDATION_GROUP_ID -> {
+                                            // 指定した値が正しくない
+                                            ApiError.showToast(
+                                                this,
+                                                errorArray.getString(i),
+                                                Toast.LENGTH_SHORT
+                                            )
+                                        }
+                                        ApiError.VALIDATION_GROUP_NAME -> {
+                                            // 指定して値が正しくない
+                                            ApiError.showToast(
+                                                this,
+                                                errorArray.getString(i),
+                                                Toast.LENGTH_SHORT
+                                            )
+                                        }
+                                        ApiError.UNKNOWN_TOKEN -> {
+                                            // ログイントークンの検証失敗
+                                            val intent = Intent(this, SignInActivity::class.java).apply {
+                                                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                                            }
+                                            startActivity(intent)
+                                            /*
+                                            ApiError.showToast(
+                                                this,
+                                                errorArray.getString(i),
+                                                Toast.LENGTH_SHORT
+                                            )
+                                            */
+                                        }
+                                        ApiError.ALREADY_CREATE_GROUP -> {
+                                            // 既に同じグループIDが登録している場合
+                                            ApiError.showToast(
+                                                this,
+                                                errorArray.getString(i),
+                                                Toast.LENGTH_SHORT
+                                            )
+
+                                        }
+                                        ApiError.REQUIRED_PARAM -> {
+                                            // 必要な値が見つかりませんでした表示
+                                            ApiError.showToast(
+                                                this,
+                                                errorArray.getString(i),
+                                                Toast.LENGTH_SHORT
+                                            )
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }.execute(ApiParam(
+                    Api.SLIM + "group/create" ,
+                    hashMapOf("token" to token,"group_id" to groupId,"group_name" to groupName)
+                ))
+            }
+            .setNegativeButton("キャンセル", null)
+            .create()
+            .show()
+    }
+    /*
+    private fun create_group(view: View){
+        val group_id = group_id.text.toString()
+    }
+
+     */
+
+
+
+
+
+    // グループ参加
 }
