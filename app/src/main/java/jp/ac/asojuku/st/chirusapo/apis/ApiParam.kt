5b173ba00e2ba4eb29d47a687c2cc90cb9437190
raige <raigekka0128@yahoo.co.jp>
2019-11-11 11:32:45 +0900

    ログイン機能完成

--- app/src/main/java/jp/ac/asojuku/st/chirusapo/apis/ApiParam.kt
+++
@@ -1,8 +0,0 @@
-package jp.ac.asojuku.st.chirusapo.apis
-
-class ApiParam(
-    val api: String,
-    val params: HashMap<String, String> = hashMapOf(),
-    val image: ArrayList<ApiParamImage>? = null,
-    val movie: ApiParamMovie? = null
-)
